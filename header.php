fc2e3e46574f14bb80781fed11546beef12b98fe
Mbouchard93 <mariiane.bouchard12@gmail.com>
2025-01-17 08:52:48 -0500

    structure de dossier

--- header.php
+++ header.php
@@ -7,7 +7,19 @@
     <?php wp_head() ?>
 </head>
 <body>
-    <header>
-        <h1>Bonjour test</h1>
+<header>
+        <nav>
+            <div class="logo">
+                <a href="<?php echo site_url(); ?>">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/logo-header.svg" alt="Logo: Escale maritime">
+                </a>
+            </div>
+            <?php
+                wp_nav_menu( $arg = array (
+                    'menu' => 'Header',
+                    'menu_class' => 'main-navigation',
+                    'theme_location' => 'primary'
+                ));
+            ?>
+        </nav>
     </header>
-    
