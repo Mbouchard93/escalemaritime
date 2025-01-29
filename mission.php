cc915b89f701a367c82d8db6009f7ee6e3a84be9
Jorge Molano <jams1810@hotmail.com>
2025-01-29 11:04:38 -0500

    Voici la section vision

--- mission.php
+++ mission.php
@@ -26,6 +26,11 @@ Template Name: Historique et mission
         get_template_part('/templates/value');
 ?>
 
+<?php 
+        get_template_part('/templates/vision');
+
+?>
+
 </main>
 
 <?php get_footer(); ?>
