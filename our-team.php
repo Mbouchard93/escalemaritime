99f080f76b64236c4e3edc668a06b9927d09c839
Mbouchard93 <mariiane.bouchard12@gmail.com>
2025-01-31 11:05:56 -0500

    section notre équipe

--- our-team.php
+++ our-team.php
@@ -7,7 +7,9 @@ Template Name: Notre équipe
 <?php get_header(); ?>
 
 <main>
-<?php get_template_part('/templates/bloc_text_image') ?>
+<?php get_template_part('/templates/module_teams') ?>
+<?php get_template_part('/templates/teams') ?>
+
 </main>
 
 <?php get_footer(); ?>
