32e238c47ef6c7f726cb4bdf9c1e92187da502c7
Mbouchard93 <mariiane.bouchard12@gmail.com>
2025-02-12 12:30:30 -0500

    correction banners

--- templates/banner_partners.php
+++ templates/banner_partners.php
@@ -1,5 +1,5 @@
 <section class="banner_partners" aria-labelledby="partners-heading">
-    <h2>Nos partenaires</h2>
+    <h2 id="partners-heading">Nos partenaires</h2>
     <div class="slider" role="region" aria-roledescription="carousel" aria-label="Logos des partenaires"> 
         <button class="control--previous" aria-label="Diapositive précédente" aria-controls="slider_slides">
             <span aria-hidden="true" class="material-symbols-outlined">arrow_back_ios</span>
@@ -7,40 +7,38 @@
         <ul class="slider_slides" role="list">
             <?php 
                 $bannerPartners = new WP_Query([
-                    'post_type' => 'partners',
+                    'post_type'      => 'partners',
                     'posts_per_page' => -1,
                 ]);
 
+                $slide_index = 1; 
                 if ($bannerPartners->have_posts()) :
-                    while ($bannerPartners->have_posts()) : $bannerPartners->the_post();
+                    while ($bannerPartners->have_posts()) : 
+                        $bannerPartners->the_post();
+                        
                         $image = get_field('partner_logo');
-                        if ($image) :
+                        $partner_name = get_the_title(); 
+                        $url = get_field('partner_link');
+
+                        if ($image) {
                             $image_id  = $image['ID'];
                             $image_url = $image['url'];
-                            $image_alt = get_acf_image_alt($image_id, 'partner_logo');
-                            $url       = get_field('partner_link');
-            ?>
-                <li class="slider_slide">
-                    <a href="<?php echo esc_url($url); ?>" target="_blank">
-                        <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($image_alt); ?>">
-                    </a>
-                </li>
-            <?php 
-                        endif;
-                        $imageUrl = $image['url'];
-                        $url = get_field('partner_link');
-            ?>
-                <li class="slider_slide" role="group" aria-roledescription="slide" aria-label="<?php echo esc_attr($current_slide); ?>">
-                <a href="<?php echo esc_url($url); ?>" target="_blank" aria-label="Visiter le site de <?php echo esc_attr($partner_name); ?>">
-                        <figure>
-                        <img src="<?php echo esc_url($imageUrl); ?>" alt="Logo Partenaire">
-                        </figure>
-                    </a>
-                </li>
-            <?php
+                            $image_alt = !empty($image['alt']) ? $image['alt'] : 'Logo de ' . $partner_name;
+                            ?>
+                            <li class="slider_slide" role="group" aria-roledescription="slide" aria-label="Diapositive <?php echo $slide_index; ?>">
+                                <a href="<?php echo esc_url($url); ?>" target="_blank" aria-label="Visiter le site de <?php echo esc_attr($partner_name); ?>">
+                                    <figure>
+                                        <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($image_alt); ?>">
+                                    </figure>
+                                </a>
+                            </li>
+                            <?php
+                        } 
+
+                        $slide_index++;
                     endwhile;
                     wp_reset_postdata();
-                endif;
+                endif; 
             ?>
         </ul>
         <button class="control--next" aria-label="Diapositive suivante" aria-controls="slider_slides">
