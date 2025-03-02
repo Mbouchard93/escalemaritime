a7b2f2259c473b3b7c3b24ad8080e7f2e927a203
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2025-03-02 18:09:17 -0500

    Try to change date and map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,33 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var map = L.map("map", {
+  // Vérifie si une carte existe déjà et la supprime avant d'en créer une nouvelle
+  if (window.myMap) {
+    window.myMap.remove();
+  }
+
+  // Crée la carte et la stocke dans une variable globale
+  window.myMap = L.map("map", {
     center: [48.1239, -69.1766],
     zoom: 13,
     minZoom: 10, // Dézoom maximum autorisé
     maxBounds: [
-      // Limites de déplacement
-      [47.9, -69.6],
+      [47.9, -69.6], // Limites de déplacement
       [48.5, -68.8],
     ],
   });
 
+  // Ajout de la couche OpenStreetMap
   L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       '&copy; <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-  }).addTo(map);
+  }).addTo(window.myMap);
 
-  // Utilise les données globales "locations"
-  locations.forEach(function (location) {
-    var marker = L.marker(location.coords).addTo(map);
+  // Vérification si "locations" est bien défini
+  if (
+    typeof locations !== "undefined" &&
+    Array.isArray(locations) &&
+    locations.length > 0
+  ) {
+    locations.forEach(function (location) {
+      // Vérifie si les coordonnées sont valides
+      if (Array.isArray(location.coords) && location.coords.length === 2) {
+        var marker = L.marker(location.coords).addTo(window.myMap);
 
-    var popupContent = `
-        <div class="map__modal">
-          <h4>${location.name}</h4>
-          <p>${location.description}</p>
-          <a href="${location.link}" target="_blank">
-          <img class="map__modal-img" src="${location.logo}" alt="${location.name}"></a>
-        </div>
-      `;
-    marker.bindPopup(popupContent);
-  });
+        var popupContent = `
+          <div class="map__modal">
+            <h4>${location.name}</h4>
+            <p>${location.description}</p>
+            <a href="${location.link}" target="_blank">
+              <img class="map__modal-img" src="${location.logo}" alt="${location.name}">
+            </a>
+          </div>
+        `;
+        marker.bindPopup(popupContent);
+      } else {
+        console.warn("Coordonnées invalides pour:", location);
+      }
+    });
+  } else {
+    console.warn("Aucune donnée de localisation disponible.");
+  }
 });
